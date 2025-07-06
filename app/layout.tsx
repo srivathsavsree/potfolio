c8726db98fe673666149e9bc7cef4b09ee095d2a
srivathsav_sree <srivathsavthaneeru@gmail.com>
2025-07-06 19:39:33 +0530

    updated webpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'srivathsasv potfolio',
+  description: 'Professional portfolio of srivathsasv',
   generator: 'v0.dev',
 }
 
