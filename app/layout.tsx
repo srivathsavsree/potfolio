667bdf39fd2e7d246411f10ae9f9839754bf7a25
srivathsav_sree <srivathsavthaneeru@gmail.com>
2025-07-06 19:44:23 +0530

    updated potfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'srivathsasv potfolio',
   description: 'Professional portfolio of srivathsasv',
-  generator: 'v0.dev',
+  generator: 'srivathsav potfolio',
 }
 
 export default function RootLayout({
